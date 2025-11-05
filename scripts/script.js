b945c8ce47932676e714d02581d37f5b349bf15d
Mav <196895938+maruvici@users.noreply.github.com>
2025-11-05 16:19:07 +0800

    feat: Add functions to get game choices

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
-console.log("Hello, World!")
+console.log("Let's Play Rock-Paper-Scissors!")
+let humanScore = computerScore = 0;
+
+function getComputerChoice() {
+    // Get random int between 0 to 2
+    choice = Math.floor(Math.random() * 3)
+
+    switch(choice) {
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
+    }
+
+    return 0;
+}
+
+function getHumanChoice() {
+    let humanChoice;
+
+    do {
+        humanChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
+    } while(!(["rock", "paper", "scissors"].includes(humanChoice)))
+    
+    return humanChoice;
+}
